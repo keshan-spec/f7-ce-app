339d8176d7c5db8ebc22c0328b6025c6a6aded01
Keshanth Jude <keshanth64@gmail.com>
2024-08-06 10:59:05 +0100

    Improved posts fetching

--- www/js/app.js
+++ www/js/app.js
@@ -2,13 +2,23 @@
 
 var $ = Dom7
 
+
+let currentPage = 1
+const limit = 10
+const API_URL = 'https://wordpress-889362-4267074.cloudwaysapps.com/uk'
+let totalPages = 40 // Initialize with a large value, update on first fetch
+
 var app = new Framework7({
   name: 'DriveLife',
   theme: 'ios',
   //theme: 'auto',
   cache: false,
   el: '#app', // App root element
-
+  on: {
+    init: function () {
+      fetchPosts(currentPage)
+    },
+  },
   // App store
   store: store,
   // App routes
@@ -16,12 +26,7 @@ var app = new Framework7({
 })
 
 
-
 //------------------------------------------ CUSTOM ------------------------------------------//
-
-
-//GLOBAL
-
 // Action Sheet with Grid Layout
 var actionSheet = app.actions.create({
   grid: true,
@@ -57,7 +62,6 @@ document.getElementById('open-action-sheet').addEventListener('click', function 
 })
 
 //HOME (DISCOVER)
-
 // Init slider
 var swiper = app.swiper.create('.swiper-container', {
   speed: 400,
@@ -67,24 +71,12 @@ var swiper = app.swiper.create('.swiper-container', {
 
 })
 
-
 //Comments Popup
 var CommentsPopup = app.popup.create({
   el: '.comments-popup',
   swipeToClose: 'to-bottom'
 });
 
-// on document ready
-$(document).on('page:init', function (e) {
-  // do something on page init
-  console.log('page init')
-  fetchPosts(1)
-})
-
-let currentPage = 1
-const limit = 10
-const API_URL = 'https://wordpress-889362-4267074.cloudwaysapps.com/uk'
-let totalPages = 40 // Initialize with a large value, update on first fetch
 
 async function fetchPosts(page) {
   const response = await fetch(`${API_URL}/wp-json/app/v1/get-posts?page=${page}&limit=10`, {
@@ -99,94 +91,6 @@ async function fetchPosts(page) {
   totalPages = data.total_pages
   displayPosts(data.data)
 }
-
-function displayPosts(posts) {
-  const postsContainer = document.getElementById('tab-latest')
-
-    // < source src = "${mediaItem.media_url}" type = "${mediaItem.media_mime_type}" />
-
-  posts.forEach(post => {
-    const postItem = `
-          <div class="media-post">
-            <div class="media-post-content">
-              <div class="media-post-header">
-                <div class="media-post-avatar" style="background-image: url('${post.user_profile_image || 'assets/img/avatar1.jpg'}');"></div>
-                <div class="media-post-user">${post.username}</div>
-                <div class="media-post-date">${post.post_date}</div>
-              </div>
-              <div class="media-post-content">
-                <div class="swiper-container">
-                  <div class="swiper-wrapper">
-                    ${post.media.map(mediaItem => `
-                      <div class="swiper-slide">
-                        ${mediaItem.media_type === 'video' ? `
-                          <video autoplay loop muted playsinline class="video-background">
-                          </video>
-                        ` : `
-                          <img src="${mediaItem.media_url}" alt="${mediaItem.media_alt}" />
-                        `}
-                      </div>
-                    `).join('')}
-                  </div>
-                  <div class="swiper-pagination"></div>
-                </div>
-              </div>
-              <div class="media-post-actions">
-                <div class="media-post-like">
-                  <i class="icon f7-icons">${post.is_liked ? 'heart_fill' : 'heart'}</i>
-                </div>
-                <div class="media-post-comment popup-open" data-popup=".comments-popup" data-post-id="${post.id}">
-                  <i class="icon f7-icons">chat_bubble</i>
-                </div>
-                <div class="media-post-share">
-                  <i class="icon f7-icons">paperplane</i>
-                </div>
-                <div class="media-post-edit">
-                  <i class="icon f7-icons">gear_alt</i>
-                </div>
-              </div>
-              <div class="media-post-likecount">${post.likes_count} likes</div>
-              <div class="media-post-description"><strong>${post.username}</strong> • ${post.caption}
-                <span class="media-post-readmore">... more</span>
-              </div>
-              <div class="media-post-commentcount popup-open" data-popup=".comments-popup" data-post-id="${post.id}">View ${post.comments_count} comments</div>
-            </div>
-          </div>
-        `
-    postsContainer.insertAdjacentHTML('beforeend', postItem)
-  })
-
-  // Initialize swiper for the dynamically added content
-  app.swiper.create('.swiper-container', {
-    pagination: {
-      el: '.swiper-pagination',
-    },
-  })
-}
-
-// Fetch initial posts when the page is initialized
-document.addEventListener('DOMContentLoaded', () => {
-  fetchPosts(currentPage)
-})
-
-// Infinite Scroll Event
-const infiniteScrollContent = document.querySelector('.infinite-scroll-content')
-infiniteScrollContent.addEventListener('infinite', function () {
-  if (currentPage >= totalPages) {
-    app.infiniteScroll.destroy(infiniteScrollContent)
-    return
-  }
-
-  currentPage++
-  fetchPosts(currentPage)
-})
-
-// on .popup-open click
-$(document).on('click', '.popup-open', function () {
-  const postId = this.getAttribute('data-post-id')
-  fetchComments(postId)
-  CommentsPopup.open()
-})
 
 async function fetchComments(postId) {
   document.getElementById('comments-list').innerHTML = '<div class="preloader"></div>'
@@ -204,11 +108,84 @@ async function fetchComments(postId) {
   displayComments(data)
 }
 
+function displayPosts(posts) {
+  const postsContainer = document.getElementById('tab-latest')
+
+  posts.forEach(post => {
+    const post_actions = `
+     <div class="media-post-actions">
+        <div class="media-post-like">
+          <i class="icon f7-icons ${post.is_liked && 'text-red'}">${post.is_liked ? 'heart_fill' : 'heart'}</i>
+        </div>
+        <div class="media-post-comment popup-open" data-popup=".comments-popup" data-post-id="${post.id}">
+          <i class="icon f7-icons">chat_bubble</i>
+        </div>
+        <div class="media-post-share">
+          <i class="icon f7-icons">paperplane</i>
+        </div>
+        <div class="media-post-edit">
+          <i class="icon f7-icons">gear_alt</i>
+        </div>
+      </div>
+    `
+
+    const date = formatPostDate(post.post_date)
+    const postItem = `
+          <div class="media-post">
+            <div class="media-post-content">
+              <div class="media-post-header">
+                <div class="media-post-avatar" style="background-image: url('${post.user_profile_image || 'assets/img/avatar1.jpg'}');"></div>
+                <div class="media-post-user">${post.username}</div>
+                <div class="media-post-date">${date}</div>
+              </div>
+              <div class="media-post-content">
+                <div class="swiper-container">
+                  <div class="swiper-wrapper">
+                    ${post.media.map(mediaItem => `
+                      <div class="swiper-slide">
+                        ${mediaItem.media_type === 'video' ? `
+                          <video autoplay loop muted playsinline class="video-background">
+                            <source src ="${mediaItem.media_url}" type ="${mediaItem.media_mime_type}" />
+                          </video>
+                        ` : `
+                          <img src="${mediaItem.media_url}" alt="${mediaItem.media_alt}" />
+                        `}
+                      </div>
+                    `).join('')}
+                  </div>
+                  <div class="swiper-pagination"></div>
+                </div>
+              </div>
+              ${post_actions}
+              <div class="media-post-likecount">${post.likes_count} likes</div>
+              <div class="media-post-description"><strong>${post.username}</strong> • ${post.caption}
+                <span class="media-post-readmore">... more</span>
+              </div>
+              ${post.comments_count > 0 ? `<div class="media-post-commentcount popup-open" data-popup=".comments-popup" data-post-id="${post.id}">View ${post.comments_count} comments</div>` : ''}
+            </div>
+          </div>
+        `
+    postsContainer.insertAdjacentHTML('beforeend', postItem)
+  })
+
+  // Initialize swiper for the dynamically added content
+  app.swiper.create('.swiper-container', {
+    pagination: {
+      el: '.swiper-pagination',
+    },
+  })
+}
+
 function displayComments(comments) {
   const commentsContainer = document.getElementById('comments-list')
 
+  if (!comments.length) {
+    commentsContainer.innerHTML = '<div class="no-comments">No comments found</div>'
+    return
+  }
+
   comments.forEach(comment => {
-    const replyItems = comment.replies.length > 0 ?`
+    const replyItems = comment.replies.length > 0 ? `
       <div class="comment-replies" data-comment-id="${comment.id}">
             <span class="comment-replies-toggle" data-replies-count="${comment.replies.length}">
             Show ${comment.replies.length} replies
@@ -258,6 +235,60 @@ function displayComments(comments) {
     commentsContainer.insertAdjacentHTML('beforeend', commentItem)
   })
 }
+
+function formatPostDate(date) {
+  const postDate = new Date(date)
+  // show date as Just now, 1 minute ago, 1 hour ago, 1 day ago, 1 week ago, 1 month ago, 1 year ago
+  const currentDate = new Date()
+  const diff = currentDate - postDate
+  const seconds = Math.floor(diff / 1000)
+  const minutes = Math.floor(seconds / 60)
+  const hours = Math.floor(minutes / 60)
+  const days = Math.floor(hours / 24)
+  const weeks = Math.floor(days / 7)
+  const months = Math.floor(days / 30)
+
+  if (months > 0) {
+    return months === 1 ? '1 month ago' : `${months} months ago`
+  }
+
+  if (weeks > 0) {
+    return weeks === 1 ? '1 week ago' : `${weeks} weeks ago`
+  }
+
+  if (days > 0) {
+    return days === 1 ? '1 day ago' : `${days} days ago`
+  }
+
+  if (hours > 0) {
+    return hours === 1 ? '1 hour ago' : `${hours} hours ago`
+  }
+
+  if (minutes > 0) {
+    return minutes === 1 ? '1 minute ago' : `${minutes} minutes ago`
+  }
+
+  return 'Just now'
+}
+
+// Infinite Scroll Event
+const infiniteScrollContent = document.querySelector('.infinite-scroll-content')
+infiniteScrollContent.addEventListener('infinite', function () {
+  if (currentPage >= totalPages) {
+    app.infiniteScroll.destroy(infiniteScrollContent)
+    return
+  }
+
+  currentPage++
+  fetchPosts(currentPage)
+})
+
+// on .popup-open click
+$(document).on('click', '.popup-open', function () {
+  const postId = this.getAttribute('data-post-id')
+  fetchComments(postId)
+  CommentsPopup.open()
+})
 
 // on .comment-replies-toggle click
 $(document).on('click', '.comment-replies-toggle', function () {
