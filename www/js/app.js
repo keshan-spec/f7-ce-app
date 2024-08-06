e23ab70e49c80aa811bd27937fa6f00b13c903a4
Keshanth Jude <keshanth64@gmail.com>
2024-08-06 10:43:12 +0100

    Added comments section

--- www/js/app.js
+++ www/js/app.js
@@ -22,7 +22,6 @@ var app = new Framework7({
 
 //GLOBAL
 
-
 // Action Sheet with Grid Layout
 var actionSheet = app.actions.create({
   grid: true,
@@ -57,10 +56,6 @@ document.getElementById('open-action-sheet').addEventListener('click', function 
   actionSheet.open()
 })
 
-
-
-
-
 //HOME (DISCOVER)
 
 // Init slider
@@ -88,10 +83,11 @@ $(document).on('page:init', function (e) {
 
 let currentPage = 1
 const limit = 10
+const API_URL = 'https://wordpress-889362-4267074.cloudwaysapps.com/uk'
 let totalPages = 40 // Initialize with a large value, update on first fetch
 
 async function fetchPosts(page) {
-  const response = await fetch(`https://wordpress-889362-4267074.cloudwaysapps.com/uk/wp-json/app/v1/get-posts?page=${page}&limit=10`, {
+  const response = await fetch(`${API_URL}/wp-json/app/v1/get-posts?page=${page}&limit=10`, {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
@@ -100,11 +96,14 @@ async function fetchPosts(page) {
   })
 
   const data = await response.json();
+  totalPages = data.total_pages
   displayPosts(data.data)
 }
 
 function displayPosts(posts) {
   const postsContainer = document.getElementById('tab-latest')
+
+    // < source src = "${mediaItem.media_url}" type = "${mediaItem.media_mime_type}" />
 
   posts.forEach(post => {
     const postItem = `
@@ -122,7 +121,6 @@ function displayPosts(posts) {
                       <div class="swiper-slide">
                         ${mediaItem.media_type === 'video' ? `
                           <video autoplay loop muted playsinline class="video-background">
-                            <source src="${mediaItem.media_url}" type="${mediaItem.media_mime_type}" />
                           </video>
                         ` : `
                           <img src="${mediaItem.media_url}" alt="${mediaItem.media_alt}" />
@@ -137,7 +135,7 @@ function displayPosts(posts) {
                 <div class="media-post-like">
                   <i class="icon f7-icons">${post.is_liked ? 'heart_fill' : 'heart'}</i>
                 </div>
-                <div class="media-post-comment popup-open" data-popup=".comments-popup">
+                <div class="media-post-comment popup-open" data-popup=".comments-popup" data-post-id="${post.id}">
                   <i class="icon f7-icons">chat_bubble</i>
                 </div>
                 <div class="media-post-share">
@@ -151,7 +149,7 @@ function displayPosts(posts) {
               <div class="media-post-description"><strong>${post.username}</strong> • ${post.caption}
                 <span class="media-post-readmore">... more</span>
               </div>
-              <div class="media-post-commentcount popup-open" data-popup=".comments-popup">View ${post.comments_count} comments</div>
+              <div class="media-post-commentcount popup-open" data-popup=".comments-popup" data-post-id="${post.id}">View ${post.comments_count} comments</div>
             </div>
           </div>
         `
@@ -178,8 +176,94 @@ infiniteScrollContent.addEventListener('infinite', function () {
     app.infiniteScroll.destroy(infiniteScrollContent)
     return
   }
-console.log('infinite', currentPage);
 
   currentPage++
   fetchPosts(currentPage)
+})
+
+// on .popup-open click
+$(document).on('click', '.popup-open', function () {
+  const postId = this.getAttribute('data-post-id')
+  fetchComments(postId)
+  CommentsPopup.open()
+})
+
+async function fetchComments(postId) {
+  document.getElementById('comments-list').innerHTML = '<div class="preloader"></div>'
+
+  const response = await fetch(`${API_URL}/wp-json/app/v1/get-post-comments`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ user_id: 1, post_id: postId }),
+  })
+
+  const data = await response.json()
+
+  displayComments(data)
+}
+
+function displayComments(comments) {
+  const commentsContainer = document.getElementById('comments-list')
+
+  comments.forEach(comment => {
+    const replyItems = comment.replies.length > 0 ?`
+      <div class="comment-replies" data-comment-id="${comment.id}">
+            <span class="comment-replies-toggle" data-replies-count="${comment.replies.length}">
+            Show ${comment.replies.length} replies
+            </span>
+            <div class="comment-replies-container">
+              ${comment.replies.map(reply => `
+                <div class="comment">
+                  <div class="comment-profile-img" style="background-image:url('${reply.profile_image}');"></div>
+                  <div class="comment-content-container">
+                    <div class="comment-username">${reply.display_name}</div>
+                    <div class="comment-content">${reply.comment}</div>
+                    <div class="comment-actions">
+                      <div class="comment-like">
+                        <i class="icon f7-icons">${reply.liked ? 'heart_fill' : 'heart'}</i> <span>${reply.likes_count}</span>
+                      </div>
+                      <div class="comment-reply">
+                        <i class="icon f7-icons">chat_bubble</i> <span>Reply</span>
+                      </div>
+                    </div>
+                  </div>
+                  <div class="clearfix"></div>
+                </div>
+              `).join('')}
+            </div>
+      </div>
+    ` : ''
+
+    const commentItem = `
+      <div class="comment">
+        <div class="comment-profile-img" style="background-image:url('${comment.profile_image}');"></div>
+        <div class="comment-content-container">
+          <div class="comment-username">${comment.display_name}</div>
+          <div class="comment-content">${comment.comment}</div>
+          <div class="comment-actions">
+            <div class="comment-like">
+              <i class="icon f7-icons">${comment.liked ? 'heart_fill' : 'heart'}</i> <span>${comment.likes_count}</span>
+            </div>
+            <div class="comment-reply">
+              <i class="icon f7-icons">chat_bubble</i> <span>Reply</span>
+            </div>
+          </div>
+          ${replyItems}
+        </div>
+        <div class="clearfix"></div>
+      </div>
+    `
+    commentsContainer.insertAdjacentHTML('beforeend', commentItem)
+  })
+}
+
+// on .comment-replies-toggle click
+$(document).on('click', '.comment-replies-toggle', function () {
+  const commentRepliesContainer = this.nextElementSibling
+  commentRepliesContainer.classList.toggle('show')
+  const repliesCount = this.getAttribute('data-replies-count')
+
+  this.innerText = this.innerText === `Show ${repliesCount} replies` ? `Hide ${repliesCount} replies` : `Show ${repliesCount} replies`
 })
