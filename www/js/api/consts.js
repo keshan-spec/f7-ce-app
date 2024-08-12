5025b8660d22f6e461b49ed4f7396c4155248023
Keshanth Jude <keshanth64@gmail.com>
2024-08-12 12:57:46 +0100

    logs

--- www/js/api/consts.js
+++ www/js/api/consts.js
@@ -8,11 +8,14 @@ export const sendRNMessage = ({
     association_type
 }) => {
     if (typeof window.ReactNativeWebView === 'undefined') {
+        alert(`This is not a react native webview, failed to send message: ${type} - ${user_id}`)
         console.warn(`This is not a react native webview, failed to send message: ${type} - ${user_id}`)
     }
 
     try {
         if (window !== undefined && window.ReactNativeWebView) {
+            alert(`Sending message: ${type} - ${user_id}`)
+
             window.ReactNativeWebView.postMessage(JSON.stringify({
                 type,
                 page,
@@ -20,8 +23,12 @@ export const sendRNMessage = ({
                 association_id,
                 association_type
             }))
+        } else {
+            alert(`Failed to send message: ${type} - ${user_id}`)
+            console.warn(`Failed to send message: ${type} - ${user_id}`)
         }
     } catch (e) {
         console.error(e)
+        alert(`Failed to send message: ${type} - ${user_id}`)
     }
 }
